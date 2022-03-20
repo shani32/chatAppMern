c6eb6d8e433cc2f4afebd6220d648072b24a6200
shani barak <shanibrk@gmail.com>
2022-03-20 14:59:22 +0200

    added bootstrap basic home page and login

--- chat-front/src/components/Navigation.js
+++ chat-front/src/components/Navigation.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Nav, Navbar, Container, Button, NavDropdown} from "react-bootstrap"
 import {LinkContainer} from 'react-router-bootstrap'
+import logo from '../assets/logo.jpg'
 
 function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">Chat App</Navbar.Brand>
+      <LinkContainer to='/'>
+      <Navbar.Brand >
+        <img src={logo} style={{width:50, height:50}}/>
+        </Navbar.Brand>
+      </LinkContainer>
+      
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
--- chat-front/src/pages/Home.js
+++ chat-front/src/pages/Home.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Row, Button, Col} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+import './Home.css'
 
 function Home() {
   return (
-    <div>home</div>
+    <Row>
+      <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+        <div>
+          <h1>Sharing things no concern</h1>
+          <p>Chat App that allows you connections</p>
+          <LinkContainer to='/chat'>
+           <Button variant='success'>Get started<i className='fas fa-comments home-message-icon'></i></Button>
+           
+          </LinkContainer>
+        </div>
+      </Col>
+     <Col md={6} className="home_bg">
+     </Col>
+    </Row>
   )
 }
 
