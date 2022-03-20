d74ef800b134b770245271a4890175879f431958
shani barak <shanibrk@gmail.com>
2022-03-20 13:39:23 +0200

    first commit

---
+++ chat-front/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+   <BrowserRouter>
+    <Navigation/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+
+     
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
