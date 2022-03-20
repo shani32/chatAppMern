b94beb03738274ad580abb9d72747c56e8826029
shani barak <shanibrk@gmail.com>
2022-03-20 20:21:00 +0200

    adding sign up page and upload img with cloudinary

--- chat-front/src/pages/Signup.js
+++ chat-front/src/pages/Signup.js
@@ -1,8 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Form, Row, Col, Button } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import './Signup.css'
+import bot from '../assets/bot.jpg'
+
+
 
 function Signup() {
-  return (
-    <div>Signup</div>
+  const [name, setName]=useState('')
+  const [email, setEmail]= useState('')
+  const [password, setPassword]= useState('')
+
+  //image upload state
+  const [image, setImage]= useState('')
+  const [uploadingImg, setUploadingImg]=useState(false)
+  const [imagePreview, setImagePreview]= useState(null)
+
+function validateImg(e){
+  const file=e.target.files[0];
+  if(file.size>=1048576){
+  return alert('max file size is 1mb')
+  }else{
+    setImage(file)
+    setImagePreview(URL.createObjectURL(file))
+  }
+}
+async function uploadImage(){
+  const data= new FormData();
+  data.append('file', image);
+  data.append('upload_preset', 'z5euge5y')
+  try{
+    setUploadingImg(true);
+    let res= await fetch('https://api.cloudinary.com/v1_1/dcirx7qjo/image/upload',{
+      method:'post',
+      body:data
+    })
+    const urlData= await res.json();
+    setUploadingImg(false)
+    return urlData.url
+  }catch(error){
+    setUploadingImg(false)
+    console.log(error)
+  }
+}
+async function handleSignup(e){
+  e.preventDefault()
+  if(!image) return alert('please upload your profile picture')
+  const url= await uploadImage(image);
+  console.log(url)
+
+}
+    return (
+    <Container>
+    <Row>
+   
+    
+    <Col md={6} className="d-flex align-items-center justify-content-center flex-direction-column">
+        <Form style={{width:'80%', maxWidth:500}} onSubmit={handleSignup}>
+          <h1 className='text-center'>Create account</h1>
+          <div className='signup-profile-pic-container'>
+            <img src={imagePreview || bot} className='signup-profile-pic'/>
+            <label htmlFor='image-upload' className='image-upload-label'>
+              <i className='fas fa-plus-circle add-picture-icon'></i>
+            </label>
+            <input type="file" id="image-upload" hidden accept="image/png, image/jpeg" onChange={validateImg}/>
+          </div>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter name" onChange={(e)=> setName(e.target.value)} value={name}/>
+         
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={(e)=> setEmail(e.target.value)} value={email}/>
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onChange={(e)=> setPassword(e.target.value)} value={password}/>
+      </Form.Group>
+     
+      <Button variant="primary" type="submit">
+       {uploadImage ? "signing you up..": "signup"}
+      </Button>
+      <div className='py-4'>
+        <p className='text-center'>
+          Already have an account ? <Link to="/login">Login</Link></p>
+      </div>
+      </Form>
+    </Col>
+    <Col md={6} className="signup_bg"></Col>
+</Row>
+</Container>
   )
 }
 
